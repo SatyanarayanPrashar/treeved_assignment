f663c997a10550d5504a3ca3bb7a50ecb9f075de
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-09-16 18:23:04 +0530

    bottomNaavigationBar done

--- lib/Screens/discover.dart
+++ lib/Screens/discover.dart
@@ -14,6 +14,14 @@ List<String> tabs = ["People", "Pages", "Lists", "Movies", "Books", "Art"];
 int current = 0;
 
 class _DiscoverPageState extends State<DiscoverPage> {
+  // int _selectedIndex = 0;
+
+  // void _onItemTapped(int index) {
+  //   setState(() {
+  //     _selectedIndex = index;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,53 +35,135 @@ class _DiscoverPageState extends State<DiscoverPage> {
         ),
         actions: const [Icon(Icons.search)],
       ),
-      body: Container(
-        margin: const EdgeInsets.all(5),
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: tabs.length,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        current = index;
-                      });
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(5),
-                      width: 90,
-                      decoration: BoxDecoration(
-                        color: current == index ? Colors.blue : Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          color: current == index ? Colors.blue : Colors.grey,
+      //
+      body: Padding(
+        padding: const EdgeInsets.only(left: 11, right: 11),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              const SizedBox(height: 7),
+              SizedBox(
+                height: 45,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: tabs.length,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          current = index;
+                        });
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 90,
+                        decoration: BoxDecoration(
+                          color: current == index ? Colors.blue : Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: current == index ? Colors.blue : Colors.grey,
+                          ),
                         ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          tabs[index],
-                          style: GoogleFonts.roboto(
-                            color:
-                                current == index ? Colors.white : Colors.grey,
-                            fontWeight: FontWeight.w600,
+                        child: Center(
+                          child: Text(
+                            tabs[index],
+                            style: GoogleFonts.roboto(
+                              color:
+                                  current == index ? Colors.white : Colors.grey,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-            peopleTab(),
-          ],
+              const SizedBox(height: 10),
+              Container(
+                color:
+                    const Color.fromARGB(255, 209, 209, 209).withOpacity(0.1),
+                height: 240,
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: const [
+                      PeopleTabs(
+                        name: 'Sam Sharma',
+                        about: 'Designer/Animator/Feminist',
+                      ),
+                      PeopleTabs(
+                        name: 'Vaibhav Raj',
+                        about: 'Entrepreneur/Founder: Risk',
+                      ),
+                      PeopleTabs(
+                        name: 'Sam Sharma',
+                        about: 'Designer/Animator/Feminist',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  const Icon(Icons.table_chart_outlined),
+                  const SizedBox(width: 20),
+                  Text("Pages you may like",
+                      style: GoogleFonts.roboto(
+                          fontSize: 17, fontWeight: FontWeight.w400)),
+                  const Spacer(),
+                  TextButton(
+                      onPressed: () {
+                        //
+                      },
+                      child: const Text("See all"))
+                ],
+              ),
+              Container(
+                color:
+                    const Color.fromARGB(255, 209, 209, 209).withOpacity(0.1),
+                height: 240,
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: const [
+                      Pagestabs(
+                        name: 'Microanimations',
+                      ),
+                      Pagestabs(
+                        name: 'Cinematography and Design',
+                      ),
+                      Pagestabs(
+                        name: 'Art Club',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  const Icon(Icons.list),
+                  const SizedBox(width: 20),
+                  Text("Explore Lists",
+                      style: GoogleFonts.roboto(
+                          fontSize: 17, fontWeight: FontWeight.w400)),
+                  const Spacer(),
+                  TextButton(
+                      onPressed: () {
+                        //
+                      },
+                      child: const Text("See more"))
+                ],
+              ),
+              const ExploreListTabs()
+            ],
+          ),
         ),
       ),
     );
